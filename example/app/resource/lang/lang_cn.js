bd2b5f80b4b69f11dd2adb2d909c3a199637e754
anyunfei <anyunfei@wanda.cn>
2017-04-11 15:48:48 +0800

    add tag component

--- example/app/resource/lang/lang_cn.js
+++ example/app/resource/lang/lang_cn.js
@@ -186,5 +186,5 @@ export default Object.assign(_defaultLang, {
   NavMenuApiDecTextAttr: '指定节点文字字段',
   NavMenuApiDecIconAttr: '指定节点图标字段',
   NavMenuApiDecMatchCurrentUrl: '当前节点和location.href的匹配情况，匹配返回true，否则返回false',
-
+  TagDes: '用于标记和选择',
 })
